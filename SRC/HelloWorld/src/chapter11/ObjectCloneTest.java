a08892044ca225aeddb12391a18028320328ce58
안지애 <rozioh@naver.com>
2024-05-24 13:06:37 +0900

    주석 추가
    Deep Copy, Shallow Copy

--- SRC/HelloWorld/src/chapter11/ObjectCloneTest.java
+++ SRC/HelloWorld/src/chapter11/ObjectCloneTest.java
@@ -39,9 +39,14 @@ public class ObjectCloneTest {
 	public static void main(String[] args) throws CloneNotSupportedException{
 		Circle circle = new Circle(10, 20, 30);
 		Circle copyCircle = (Circle)circle.clone();
+//		Circle copyCircle = circle; // Shallow Copy
 		
 		System.out.println(circle);
 		System.out.println(copyCircle);
+		// Hash 값이 다른 것은 두 개가 다른 값이다. Deep Copy
+		// Deep Copy 방법
+		// 1. cloneable 구현
+		// 2. 재정의 
 		System.out.println(System.identityHashCode(circle));
 		System.out.println(System.identityHashCode(copyCircle));
 	}
