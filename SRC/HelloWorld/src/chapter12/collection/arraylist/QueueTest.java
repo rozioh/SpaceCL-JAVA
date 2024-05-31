e522043363c52a60cb8465089d41315e03fd088c
안지애 <rozioh@naver.com>
2024-05-31 10:05:02 +0900

    Ch12. ArrayList를 활용한 QueueTest

---
+++ SRC/HelloWorld/src/chapter12/collection/arraylist/QueueTest.java
@@ -0,0 +1,33 @@
+package chapter12.collection.arraylist;
+
+import java.util.ArrayList;
+
+class MyQueue{
+	private ArrayList<String> arrayQueue = new ArrayList<String>();
+	
+	public void enQuene(String data) {
+		arrayQueue.add(data);
+	}
+	
+	public String deQueue() {
+		int len = arrayQueue.size();
+		if(len == 0) {
+			System.out.println("큐가 비었습니다.");
+			return null;
+		}
+		return (arrayQueue.remove(0));
+	}
+}
+public class QueueTest {
+	public static void main(String[] args) {
+		MyQueue queue = new MyQueue();
+		
+		queue.enQuene("A");
+		queue.enQuene("B");
+		queue.enQuene("C");
+		
+		System.out.println(queue.deQueue());
+		System.out.println(queue.deQueue());
+		System.out.println(queue.deQueue());
+	}
+}
